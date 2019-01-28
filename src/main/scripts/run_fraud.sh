bc00d994c0200c6c8d9d01e3aa2714e774a00c7c
Philip Xiao <FX36019@ybgweb.ny.ssmb.com>
2019-01-28 13:35:46 -0500

    add new user case

--- src/main/scripts/run_fraud.sh
+++ src/main/scripts/run_fraud.sh
@@ -16,6 +16,8 @@ hdfs_folder_check "source/mapreducejava"
 hdfs_folder_clean "source/mapreducejava/fraud"
 hdfs_folder_clean "source/mapreducejava/fraud/output"
 
+export $MAPREDUCEJAVA_JAR=$REPO/com/yieldbook/MapReduceJava/2.0.0/MapReduceJava-2.0.0-shaded.jar
+
 
 cd /usr/book/data/evenodd
 
@@ -24,4 +26,4 @@ hadoop fs -put fraud.txt /user/oozie/source/mapreducejava/fraud
 inputPath='/user/oozie/source/mapreducejava/fraud/fraud.txt'
 outputPath='/user/oozie/source/mapreducejava/fraud/output'
 
-hadoop jar $HBASEJAVA_JAR com.yieldbook.mortgage.hadoop.mapReduce.fraud.FraudDetectionDriver $inputPath $outputPath
+hadoop jar $MAPREDUCEJAVA_JAR com.yieldbook.mortgage.hadoop.mapReduce.fraud.FraudDetectionDriver $inputPath $outputPath
