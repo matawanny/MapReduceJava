570acaa684d48d02452cd2874049705b0075199d
Philip Xiao <FX36019@ybgweb.ny.ssmb.com>
2019-01-28 13:29:19 -0500

    add new user case

--- src/main/scripts/run_even_odd.sh
+++ src/main/scripts/run_even_odd.sh
@@ -13,15 +13,15 @@ fi
 source $REPO/scripts/common.sh
 hdfs_folder_check "source"
 hdfs_folder_check "source/mapreducejava"
-hdfs_folder_clean "source/mapreducejava/fraud"
-hdfs_folder_clean "source/mapreducejava/fraud/output"
+hdfs_folder_clean "source/mapreducejava/evenodd"
+hdfs_folder_clean "source/mapreducejava/evenodd/output"
 
 
 cd /usr/book/data/evenodd
 
-hadoop fs -put fraud.txt /user/oozie/source/mapreducejava/fraud
+hadoop fs -copyFromLocal evenodd.txt /user/oozie/source/mapreducejava/evenodd
 
-inputPath='/user/oozie/source/mapreducejava/fraud/fraud.txt'
-outputPath='/user/oozie/source/mapreducejava/fraud/output'
+inputPath='/user/oozie/source/mapreducejava/evenodd/evenodd.txt'
+outputPath='/user/oozie/source/mapreducejava/evenodd/output'
 
-hadoop jar $HBASEJAVA_JAR com.yieldbook.mortgage.hadoop.mapReduce.fraud.FraudDetectionDriver $inputPath $outputPath
+hadoop jar $HBASEJAVA_JAR com.yieldbook.mortgage.hadoop.mapReduce.evenodd.MyDriver $inputPath $outputPath
--- src/main/scripts/run_fraud.sh
+++ src/main/scripts/run_fraud.sh
@@ -13,15 +13,15 @@ fi
 source $REPO/scripts/common.sh
 hdfs_folder_check "source"
 hdfs_folder_check "source/mapreducejava"
-hdfs_folder_clean "source/mapreducejava/evenodd"
-hdfs_folder_clean "source/mapreducejava/evenodd/output"
+hdfs_folder_clean "source/mapreducejava/fraud"
+hdfs_folder_clean "source/mapreducejava/fraud/output"
 
 
 cd /usr/book/data/evenodd
 
-hadoop fs -copyFromLocal evenodd.txt /user/oozie/source/mapreducejava/evenodd
+hadoop fs -put fraud.txt /user/oozie/source/mapreducejava/fraud
 
-inputPath='/user/oozie/source/mapreducejava/evenodd/evenodd.txt'
-outputPath='/user/oozie/source/mapreducejava/evenodd/output'
+inputPath='/user/oozie/source/mapreducejava/fraud/fraud.txt'
+outputPath='/user/oozie/source/mapreducejava/fraud/output'
 
-hadoop jar $HBASEJAVA_JAR com.yieldbook.mortgage.hadoop.mapReduce.evenodd.MyDriver $inputPath $outputPath
+hadoop jar $HBASEJAVA_JAR com.yieldbook.mortgage.hadoop.mapReduce.fraud.FraudDetectionDriver $inputPath $outputPath
